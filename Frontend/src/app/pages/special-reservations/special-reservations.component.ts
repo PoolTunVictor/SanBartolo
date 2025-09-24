c3af53c048e30f9cbe65b207e7d99e1090fab107
Katia Uc <katiaucdzib@gmail.com>
2025-09-24 16:07:17 -0600

    Se restauro la rama

---
+++ Frontend/src/app/pages/special-reservations/special-reservations.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-special-reservations',
+  imports: [],
+  templateUrl: './special-reservations.component.html',
+  styleUrl: './special-reservations.component.css'
+})
+export class SpecialReservationsComponent {
+
+}
